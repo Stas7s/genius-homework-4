b35b27ea2a3171dc5de7c5496c737d7695cd5c74
Stas7s <stas.druk@gmail.com>
2024-03-20 17:19:13 +0200

    Add mobile menu. Adaptive for header and hero-section

---
+++ js/mobile-menu.js
@@ -0,0 +1,11 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuBtnOpen = document.querySelector(".menu-btn-open");
+const menuBtnClose = document.querySelector(".menu-btn-close");
+
+const toggleMenu = () => mobileMenu.classList.toggle("is-open");
+
+menuBtnOpen.addEventListener("click", toggleMenu);
+menuBtnClose.addEventListener("click", toggleMenu);
+
+menuBtnOpen.addEventListener("click", disableScroll);
+menuBtnClose.addEventListener("click", disableScroll);
