8010155dfd81e26020480751768f62d79eb23c0f
Stas7s <stas.druk@gmail.com>
2024-04-10 10:52:10 +0300

    Adaptive for footer.

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,6 +4,9 @@ const menuBtnClose = document.querySelector(".menu-btn-close");
 
 const toggleMenu = () => mobileMenu.classList.toggle("is-open");
 
+const disableScroll = () =>
+  document.body.classList.toggle("is-scroll-disabled");
+
 menuBtnOpen.addEventListener("click", toggleMenu);
 menuBtnClose.addEventListener("click", toggleMenu);
 
